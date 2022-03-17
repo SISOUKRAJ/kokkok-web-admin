8694387e7db1ae7ebe400f2ebd1280d6d7991409
Souk <souk2400@gmail.com>
2022-03-17 15:55:37 +0700

    :: ADD :: api get all license plate && price && type_second

--- src/views/context/getCarOption.js
+++ src/views/context/getCarOption.js
@@ -5,13 +5,24 @@ import React, {
 } from "react";
 import axios from "axios";
 
-export const CarOptionContext = createContext({ cars: [], car_type: [], car_brands: [], car_models: [] });
+export const CarOptionContext = createContext({
+    cars: [],
+    car_type: [],
+    car_type_second: [],
+    car_brands: [],
+    car_models: [],
+    car_license_plate: [],
+    car_price: [],
+});
 
 const GetCarOption = (props) => {
     const [cars, setCars] = useState([]);
     const [car_type, setCarType] = useState([]);
+    const [car_type_second, setCarTypeSecond] = useState([]);
     const [car_brands, setCarBrands] = useState([]);
     const [car_models, setCarModels] = useState([]);
+    const [car_price, setCarPrice] = useState([]);
+    const [car_license_plate, setCarLicensePlate] = useState([]);
 
     const get_cars = async () => {
         await axios.get(`${process.env.REACT_APP_API_URL_V1}/api/v1/admin/oauth/car`,
@@ -35,6 +46,19 @@ const GetCarOption = (props) => {
             })
             .then(res => {
                 setCarType(res.data.data);
+                // console.log(res.data.data);
+            })
+            .catch((err) => console.log(err))
+    }
+    const get_car_type_second = async () => {
+        await axios.get(`${process.env.REACT_APP_API_URL_V1}/api/v1/admin/oauth/car-type-second`,
+            {
+                headers: {
+                    "Authorization": `Bearer ${localStorage.getItem("token")}`,
+                },
+            })
+            .then(res => {
+                setCarTypeSecond(res.data.data);
                 // console.log(res.data.data);
             })
             .catch((err) => console.log(err))
@@ -66,11 +90,42 @@ const GetCarOption = (props) => {
             .catch((err) => console.log(err))
     }
 
+    const get_car_price = async () => {
+        await axios.get(`${process.env.REACT_APP_API_URL_V1}/api/v1/admin/oauth/car-price`,
+            {
+                headers: {
+                    "Authorization": `Bearer ${localStorage.getItem("token")}`,
+                },
+            })
+            .then(res => {
+                setCarPrice(res.data.data);
+                // console.log(res.data.data);
+            })
+            .catch((err) => console.log(err))
+    }
+
+    const get_car_license_plate = async () => {
+        await axios.get(`${process.env.REACT_APP_API_URL_V1}/api/v1/admin/oauth/car-license-plate`,
+            {
+                headers: {
+                    "Authorization": `Bearer ${localStorage.getItem("token")}`,
+                },
+            })
+            .then(res => {
+                setCarLicensePlate(res.data.data);
+                // console.log(res.data.data);
+            })
+            .catch((err) => console.log(err))
+    }
+
     useEffect(() => {
         get_cars();
         get_car_type();
+        get_car_type_second();
         get_car_brands();
         get_car_models();
+        get_car_price();
+        get_car_license_plate();
     }, []);
 
     // console.log("drivers", drivers);
@@ -82,7 +137,7 @@ const GetCarOption = (props) => {
     return (
         <div>
             <CarOptionContext.Provider
-                value={{ cars, car_brands, car_models, car_type }}
+                value={{ cars, car_brands, car_models, car_type, car_type_second, car_price, car_license_plate }}
             >
                 {props.children}
             </CarOptionContext.Provider>
